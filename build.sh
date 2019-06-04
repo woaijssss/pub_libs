8c7f3a005f9603f31bd83f132c1662057e445db8
woaijssss <wenhanf@163.com>
2019-06-05 02:23:04 +0800

    增加数据库安装、添加辅助工具

--- build.sh
+++ build.sh
@@ -20,27 +20,12 @@ JSONCPP="jsoncpp-src-"$JSONCPP_VER
 LOG4CPLUS="log4cplus-"$LOG4CPLUS_VER
 REDIS="redis-"$REDIS_VER
 SCONS="scons-"$SCONS_VER
- 
-NEW_SH="install"
-if [ $0 == "./build.sh" ] ; then
-	cd apps
-	if [ ! -e "shc-3.8.9b.tgz" ] ; then
-		wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9b.tgz
-	fi
-	
-	tar -zxf shc-3.8.9b.tgz
-	cd shc-3.8.9b
-	make
-	cd $ROOT_DIR
 
-	# 加密shell脚本
-	shc -rf $0
-	mv $0".x" $NEW_SH
-	rm -f build*
-	
-	./$NEW_SH
-	#rm -f $0
-fi
+#----------------------------------------------------------------------------------------------
+# 标识是否需要安装对应的数据库
+MYSQL=0
+REDIS=0
+#----------------------------------------------------------------------------------------------
 
 # 普通的编译函数
 function Make()
@@ -100,21 +85,83 @@ function installLibs()
 # 安装数据库
 function installDBs()
 {
-	# 安装mysql
-	wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-	rpm -ivh mysql-community-release-el7-5.noarch.rpm
-	yum update
-	yum install mysql-server
-	chown mysql:mysql -R /var/lib/mysql
-	mysqld --initialize
-	systemctl start mysqld
-	sleep 1
-	systemctl status mysqld
+	cd databases
+	if [ ${REDIS} == 1 ] ; then
+		# 安装redis
+		#wget http://download.redis.io/releases/${REDIS}${TARGZ_SUFFIX}
+		#tar -zxvf ${REDIS}${TARGZ_SUFFIX}
+		#cd ${REDIS}
+		#make BUILD=release
+		#mkdir /usr/redis  
+		#cp src/redis-server /usr/redis  
+		#cp src/redis-benchmark /usr/redis  
+		#cp src/redis-cli /usr/redis  
+		#cp redis.conf /usr/redis  
+		
+		#echo "/usr/redis/redis-server /usr/redis/redis.conf &" > /etc/init.d/redis_start.sh
+		#chmod 755 /etc/init.d/redis_start.sh
+		#ln -sf /etc/init.d/redis_start.sh /etc/rc.d/rc2.d/S100redis_start
+		#ln -sf /etc/init.d/redis_start.sh /etc/rc.d/rc3.d/S100redis_start
+		#ln -sf /etc/init.d/redis_start.sh /etc/rc.d/rc4.d/S100redis_start
+		#ln -sf /etc/init.d/redis_start.sh /etc/rc.d/rc5.d/S100redis_start
+		echo "install redis"
+	fi
 	
-	# 安装redis
+	if [ ${MYSQL} == 1 ] ; then
+		# 安装mysql
+		#wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+		#rpm -ivh mysql-community-release-el7-5.noarch.rpm
+		#yum update -y
+		#yum install -y mysql-server
+		#chown mysql:mysql -R /var/lib/mysql	# 这个好像有问题
+		#chown mysql.mysql /var/run/mysqld/
+		#mysqld --initialize
+		#systemctl start mysqld
+		#sleep 1
+		#systemctl status mysqld
+		echo "install mysql"
+	fi
+	
+	cd $ROOT_DIR
 }
 
-installLibs
+
+if [ $# -lt 2 ] ; then
+	echo "Usage such as: ./build.sh 0 0"
+	exit 0
+fi
+
+NEW_SH="install"
+if [ $0 == "./build.sh" ] ; then
+	cd apps
+	if [ ! -e "shc-3.8.9b.tgz" ] ; then
+		wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9b.tgz
+		tar -zxf shc-3.8.9b.tgz
+		cd shc-3.8.9b
+		make
+	fi
+	cd $ROOT_DIR
+
+	# 加密shell脚本
+	shc -rf $0
+	mv $0".x" $NEW_SH
+	rm -f build.sh.x.c
+	#rm -f build*
+	
+	./$NEW_SH $1 $2
+	#rm -f $0
+	exit 0
+fi
+
+if [ $1 -eq 1 ] ; then
+	REDIS=1
+fi
+
+if [ $2 -eq 1 ] ; then
+	MYSQL=1
+fi
+	
+#installLibs
 installDBs
 
 
